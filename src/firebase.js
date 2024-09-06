0c3d38f448af96d5e59e03210a071f5e56b7a9b6
fy35 <fatihy1925@gmail.com>
2024-09-06 19:29:01 +0300

    last

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEqE0_Aa6dhRNHYNQuLnq0aLJD7USJcIg",
-  authDomain: "guidecreator-7a37f.firebaseapp.com",
-  projectId: "guidecreator-7a37f",
-  storageBucket: "guidecreator-7a37f.appspot.com",
-  messagingSenderId: "99516860843",
-  appId: "1:99516860843:web:550daef61adc9867292aee",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Firebase'i başlat
